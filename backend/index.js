2d48f3393f59168c9752bb42665bcb4e39097340
anu1818 <anuragwadikar568@gmail.com>
2023-10-24 12:22:29 +0530

    Start of New Project

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+import userRouter from "./routes/User_Route.js";
+import adminRouter from "./routes/Admin_Route.js";
+import movieRouter from "./routes/Movies_Router.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.port_no;
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+import { DB_CONNECT } from "./config/database.js";
+DB_CONNECT();
+
+app.listen(PORT, () => {
+  console.log(`Server Started Successfully at ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`<h1>My home Page ..yes!!!</h1>`);
+});
